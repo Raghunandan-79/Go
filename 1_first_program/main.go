d7e3246e71e50a735e52fbe46d3f869fd2efbf3a
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-31 23:41:09 +0530

    first program

--- 1_first_program/main.go
+++ 1_first_program/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+/*
+	5 reasons to choose Golang:
+		- Build time
+		- Fast startup
+		- Performance and Efficiency
+		- Concurrency Model
+		- Static typing and compilation
+*/
+
+
 func main() {
 	fmt.Println("Namaste duniya")
 }
